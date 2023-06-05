9a64779743cbdcf82006ebb11678f5d5cc86b554
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-06-05 11:13:05 +0200

    add opposite number js

---
+++ 03-opposite-number.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// function opposite(number) {
+//   //your code here
+// }
+
+function opposite(number) {
+  return -number;
+}
