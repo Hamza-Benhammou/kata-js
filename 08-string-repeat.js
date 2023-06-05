575e796daee20441adccea2ee171627ca3c5261a
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-06-05 14:59:29 +0200

    string repeat

---
+++ 08-string-repeat.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// function repeatStr (n, s) {
+//   return '';
+// }
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
