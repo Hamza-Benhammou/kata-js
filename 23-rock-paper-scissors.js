34a9c58d2c0402757093af9e2862064308e3460e
Hamza-Benhammou <haamzaa.be@gmail.com>
2023-06-06 16:02:10 +0200

    fix rock paper scissors

--- 23-rock-paper-scissors.js
+++ 23-rock-paper-scissors.js
@@ -13,12 +13,12 @@
 // }
 
 const rps = (p1, p2) => {
-    switch (p1, p2) {
-     
-     case p1 === "scissors" && p2 === "paper" : 
-        return "Player 1 won!"
-    } 
-    }
+    if (p1 === p2) return 'Draw!';
+    if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+    if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+    if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+    return 'Player 2 won!';
+  };
    
 
  
